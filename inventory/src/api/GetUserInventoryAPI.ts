2ec9a1b650dfd2e93799d9dfc562acf020ed6b67
jinpingq <JINPINGQ@UCI.EDU>
2024-05-10 15:00:14 -0700

    add inventory in Product

--- inventory/src/api/GetUserInventoryAPI.ts
+++ inventory/src/api/GetUserInventoryAPI.ts
@@ -10,7 +10,7 @@ const newProduct = (): Product => {
   };
 };
 
-export async function getUserInventory(userId: string | undefined): Promise<Array<Product>> {
+export async function getUserInventory(_userId: string | undefined): Promise<Array<Product>> {
   return new Promise<Product[]>((resolve) => {
       // Simulating an asynchronous operation
       // (e.g., fetching data)
@@ -26,7 +26,7 @@ export async function getUserInventory(userId: string | undefined): Promise<Arra
   });
 }
 
-export async function addProductToUserInventory(productId: string | undefined): Promise<boolean> {
+export async function addProductToUserInventory(_productId: string | undefined): Promise<boolean> {
   return new Promise<boolean>((resolve) => {
       // Simulating an asynchronous operation
       // (e.g., fetching data)
